ecd9b51de9bde29ba337290778c50c8eb202136d
Kryno Bosman <83137888+Kryno@users.noreply.github.com>
2022-03-28 16:19:15 +0200

    Add files via upload
    A wrapper for the tool minicom. This tool writes the capture files to a specific directory. Say you need to console to a machine that is of a certain time and you wish to keep that separated from the rest, you could use this wrapper. 
    
    $ console.py -p AMD umpalumpa
    
    This would create a directory with the name 'AMD' and that directory would contain a file with the name umpalumpa_2022-03-28_16:17:13.capture.

---
+++ console.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python3
+"""Capture minicom output.
+
+A wrapper for the tool minicom to capture the output just a little more
+specific.
+
+The capture file will be store in the following format.
+
+~ -+- general   -+- eear1
+   |             +- erbr9
+   |
+   +- projectXY -+- ejbr1
+                 +- ejbr10
+"""
+
+__author__ = "Kryno Bosman"
+__version__ = "1.0"
+__date__ = "2022-03-29"
+
+
+import os
+import os.path
+import subprocess
+import argparse
+import datetime
+
+
+def main():
+    """Main logic."""
+    parser = argparse.ArgumentParser()
+    parser.add_argument('name',
+        help = 'machine name')
+    parser.add_argument('-p', '--product',
+        default = 'general',
+        help = 'product or platform')
+    parser.add_argument('-D', '--device',
+        default = '/dev/ttyUSB0',
+        help = 'set device name')
+    parser.add_argument('-b', '--baudrate',
+        default = '115200',
+        help = 'set baudrate')
+    args = parser.parse_args()
+
+    # Date and time.
+    date = datetime.date.today().strftime("%Y-%m-%d")
+    time = datetime.datetime.now().strftime("%H:%M:%S")
+
+    # Define a capture file in the user's home directory.
+    home_directory = os.path.expanduser("~")
+    filename = args.name + "_" +  date + "_" + time + ".capture"
+    console_directory = os.path.join(home_directory, args.product)
+    capture_file = os.path.join(console_directory, filename)
+
+    # Create environment if not exist.
+    if not os.path.exists(console_directory):
+        os.mkdir(console_directory)
+    else:
+        print(f"Directory {console_directory} exists.")
+
+
+    # Add capture file to environment.
+    cmnd = f"minicom -D {args.device} -b {args.baudrate} -C {capture_file} {args.name}"
+
+    try:
+        subprocess.run(cmnd.split(), check = True)
+    except subprocess.CalledProcessError as err:
+        print(f"Whoops! {err}")
+
+
+if __name__ == "__main__":
+    main()
